516c15eb3d7356e0a194680de8145b7c661bde35
Gachnang <markus.gachnang@gmx.ch>
2018-11-28 22:37:57 +0100

    Define Kunde

---
+++ AutoReservation.Common/Interfaces/IKunde.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoReservation.Common.Interfaces {
+    public interface IKunde {
+
+        int Id { get; set; }
+        string Nachname { get; set; }
+        string Vorname { get; set; }
+        DateTime Geburtsdatum { get; set; }
+        byte[] RowVersion { get; set; }
+    }
+}

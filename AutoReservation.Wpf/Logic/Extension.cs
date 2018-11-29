a269efeead052c5b73b27fc42c9e8e421bf1c472
Gachnang <markus.gachnang@gmx.ch>
2018-11-29 15:02:01 +0100

    Update Extension.cs

--- AutoReservation.Wpf/Logic/Extension.cs
+++ AutoReservation.Wpf/Logic/Extension.cs
@@ -18,5 +18,29 @@ namespace AutoReservation.Wpf.Logic {
                 throw new ArgumentException("Target and Source are not of the same type.");
             }
         }
+
+        /// <summary>
+        ///     Replace properties of an object with the properties of another object.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="source"></param>
+        /// <param name="type"></param>
+        public static void Replace(this object target, object source, Type type)
+        {
+            if (target.GetType().IsSubclassOf(type) && source.GetType().IsSubclassOf(type))
+            {
+                PropertyInfo[] properties = type.GetProperties();
+                foreach (PropertyInfo propertyInfo in properties)
+                {
+                    propertyInfo.SetValue(target, propertyInfo.GetValue(source));
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Target(\"" + target.GetType().Name + 
+                                            "\") or Source(\"" + source.GetType().Name + 
+                                            "\") is not subclass of defined type (\"" + type.Name + "\").");
+            }
+        }
     }
 }

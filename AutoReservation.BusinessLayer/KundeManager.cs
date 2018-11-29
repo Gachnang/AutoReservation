018345012e990635ff29924dfaabd0461b5e6ff4
SeverinAmacher <severin.amacher@gmx.net>
2018-11-29 14:56:18 +0100

    Implemented Buisness logic for Customer

--- AutoReservation.BusinessLayer/KundeManager.cs
+++ AutoReservation.BusinessLayer/KundeManager.cs
@@ -1,7 +1,64 @@
-﻿namespace AutoReservation.BusinessLayer
+﻿using AutoReservation.Dal;
+using AutoReservation.Dal.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AutoReservation.BusinessLayer
 {
     public class KundeManager
         : ManagerBase
     {
+
+        public List<Kunde> GetKunden()
+        {
+            using (AutoReservationContext context = new AutoReservationContext())
+            {
+                return context.Kunden.ToList();
+            }
+        }
+
+        public Kunde GetKunde(int id)
+        {
+            using (AutoReservationContext context = new AutoReservationContext())
+            {
+                return context.Kunden.Where(k => k.Id == id).FirstOrDefault();
+            }
+        }
+        
+        public void DeleteKunde (Kunde kunde)
+        {
+            using (AutoReservationContext context = new AutoReservationContext())
+            {
+                context.Entry<Kunde>(kunde).State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public int Insert (Kunde kunde)
+        {
+            using (AutoReservationContext context = new AutoReservationContext())
+            {
+                context.Entry<Kunde>(kunde).State = EntityState.Added;
+                context.SaveChanges();
+                return kunde.Id;
+            }
+        }
+
+        public void Update (Kunde kunde)
+        {
+            using (AutoReservationContext context = new AutoReservationContext())
+            {
+                context.Entry<Kunde>(kunde).State = EntityState.Modified;
+                try
+                {
+                    context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw CreateOptimisticConcurrencyException<Kunde>(context, kunde);
+                }
+            }
+        }
     }
 }

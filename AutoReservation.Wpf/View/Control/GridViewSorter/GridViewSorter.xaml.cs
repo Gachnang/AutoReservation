7e23e83a1103a6dab77a6c53250e217145dae811
Gachnang <markus.gachnang@gmx.ch>
2018-11-30 13:37:53 +0100

    Try to make GridViewSorter visible in Designer
    failed....
    Why?

--- AutoReservation.Wpf/View/Control/GridViewSorter/GridViewSorter.xaml.cs
+++ AutoReservation.Wpf/View/Control/GridViewSorter/GridViewSorter.xaml.cs
@@ -20,33 +20,73 @@ namespace AutoReservation.Wpf.View.Control.GridViewSorter
     /// <summary>
     /// Interaction logic for GridViewSorter.xaml
     /// </summary>
-    [ContentProperty("Columns")]
-    public partial class GridViewSorter : ListViewSorter.ListViewSorterBase
+    public partial class GridViewSorter : ListViewSorter.ListViewSorterBase, IAddChild
     {
-        
-        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
         public GridViewColumnCollection Columns {
             get => this.GridView.Columns;
         }
-        /*
+
+        /// <summary>
+        ///  Add an object child to this control
+        /// </summary>
+        void IAddChild.AddChild(object column)
+        {
+            AddChild(column);
+        }
+
+        /// <summary>
+        ///  Add an object child to this control
+        /// </summary>
+        protected override void AddChild(object column)
+        {
+            GridViewColumn c = column as GridViewColumn;
+
+            if (c != null)
+            {
+                Columns.Add(c);
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid type of child.");
+            }
+        }
+
+        /// <summary>
+        ///  Add a text string to this control
+        /// </summary>
+        void IAddChild.AddText(string text)
+        {
+            AddText(text);
+        }
+
+        /// <summary>
+        ///  Add a text string to this control
+        /// </summary>
+        protected override void AddText(string text)
+        {
+            AddChild(text);
+        }
+        
         /// <summary>
         /// This is the dependency property registered for the GridView' ColumnCollection attached property.
         /// </summary>
         public static readonly DependencyProperty ColumnCollectionProperty
-            = DependencyProperty.RegisterAttached(
-                "ColumnCollection",
-                typeof(GridViewColumnCollection),
-                typeof(GridViewSorter));
+            = System.Windows.Controls.GridView.ColumnCollectionProperty;
 
         /// <summary>
         /// Reads the attached property GridViewColumnCollection from the given element.
         /// </summary>
         /// <param name="element">The element from which to read the GridViewColumnCollection attached property.</param>
         /// <returns>The property's value.</returns>
-        public static GridViewColumnCollection GetColumnCollection(DependencyObject element)
-        {
-            if (element == null) { throw new ArgumentNullException(nameof(element)); }
-            return (GridViewColumnCollection)element.GetValue(GridViewSorter.ColumnCollectionProperty);
+        public static GridViewColumnCollection GetColumnCollection(DependencyObject element) {
+            GridViewSorter sorter = element as GridViewSorter;
+            if (sorter != null) {
+                return System.Windows.Controls.GridView.GetColumnCollection(sorter.GridView);
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid type of element.");
+            }
         }
 
         /// <summary>
@@ -56,9 +96,24 @@ namespace AutoReservation.Wpf.View.Control.GridViewSorter
         /// <param name="collection">The collection to set</param>
         public static void SetColumnCollection(DependencyObject element, GridViewColumnCollection collection)
         {
-            if (element == null) { throw new ArgumentNullException(nameof(element)); }
-            element.SetValue(ColumnCollectionProperty, collection);
-        }*/
+            GridViewSorter sorter = element as GridViewSorter;
+            if (sorter != null)
+            {
+                System.Windows.Controls.GridView.SetColumnCollection(sorter.GridView, collection);
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid type of element.");
+            }
+        }
+
+        /// <summary>
+        /// Whether should serialize ColumnCollection attach DP
+        /// </summary>
+        /// <param name="obj">Object on which this DP is set</param>
+        public static bool ShouldSerializeColumnCollection(DependencyObject obj) {
+            return System.Windows.Controls.GridView.ShouldSerializeColumnCollection(obj);
+        }
 
         static GridViewSorter() {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(GridViewSorter),

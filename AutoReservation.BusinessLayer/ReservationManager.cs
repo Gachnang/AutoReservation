04c3aa3a878c7b9ee24251517f9180f5f584439d
Kuma <michael-naef@hotmail.com>
2018-12-06 15:38:49 +0100

    ReservationManager done

--- AutoReservation.BusinessLayer/ReservationManager.cs
+++ AutoReservation.BusinessLayer/ReservationManager.cs
@@ -1,48 +1,98 @@
-﻿using AutoReservation.Dal.Entities;
+﻿using AutoReservation.BusinessLayer.Exceptions;
+using AutoReservation.Dal;
+using AutoReservation.Dal.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AutoReservation.BusinessLayer
 {
-    public class ReservationManager
-        : ManagerBase
-    {
-        // TODO: Check if Auto is available
-        public List<Reservation> GetAllReservations()
-        {
-            /*
-            * TODO: Get List of All Reservations
-            */
-            throw new NotImplementedException();
-        }
+	public class ReservationManager
+		: ManagerBase
+	{
+		// TODO: Check if Auto is available
+		public List<Reservation> List
+		{
+			get
+			{
+				using (AutoReservationContext context = new AutoReservationContext())
+				{
+					return context.Reservationen.ToList();
+				}
+			}
+		}
 
-        public Reservation GetReservation(int id)
-        {
-            /*
-             * TODO: Get Reservation
-             */
-            throw new NotImplementedException();
-        }
+		public Reservation GetReservation(int resNr)
+		{
+			using (AutoReservationContext context = new AutoReservationContext())
+			{
+				return context.Reservationen.Include(r => r.Auto).Include(r => r.Kunde)
+					.Single(res => res.ReservationsNr == resNr);
+			}
+		}
 
-        public void Insert(Reservation reservation)
-        {
-            /*
-             * TODO: Add new Reservation
-             */
-        }
+		public void InsertReservation(Reservation reservation)
+		{
+			using (AutoReservationContext context = new AutoReservationContext())
+			{
+				if (RangeCheck(reservation))
+				{
+					context.Entry(reservation).State = EntityState.Added;
+					context.SaveChanges();
+				}
+				else
+				{
+					throw new InvalidDateRangeException("Error while inserting", reservation);
+				}
+			}
+		}
 
-        public void Update(Reservation reservation)
-        {
-            /*
-             * TODO: Update Reservation
-             */
-        }
+		public void UpdateReservation(Reservation reservation)
+		{
+			//TODO RangeCheck
+			using (AutoReservationContext context = new AutoReservationContext())
+			{
 
-        public void Delete(Reservation reservation)
-        {
-            /*
-             * TODO: Delete Reservation
-             */
-        }
-    }
+				if (RangeCheck(reservation))
+				{
+					context.Entry(reservation).State = EntityState.Modified;
+					try
+					{
+						context.SaveChanges();
+					}
+					catch (DbUpdateConcurrencyException)
+					{
+						throw CreateOptimisticConcurrencyException(context, reservation);
+					}
+				}
+				else
+				{
+					throw new InvalidDateRangeException("Error while inserting", reservation);
+				}
+			}
+		}
+
+		public void DeleteReservation(Reservation reservation)
+		{
+			using (AutoReservationContext context = new AutoReservationContext())
+			{
+				context.Entry(reservation).State = EntityState.Deleted;
+				try
+				{
+					context.SaveChanges();
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e);
+					throw;
+				}
+			}
+		}
+
+		public bool RangeCheck(Reservation reservation)
+		{
+			return (reservation.Von.AddHours(24).CompareTo(reservation.Bis) < 0);
+		}
+	}
 }

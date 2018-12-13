0ffb97ac7c64fa771f043fc45ee2a309364d6edf
Gachnang <markus.gachnang@gmx.ch>
2018-12-13 13:50:41 +0100

    Init Database

--- AutoReservation.Dal/AutoReservationContext.cs
+++ AutoReservation.Dal/AutoReservationContext.cs
@@ -1,28 +1,67 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using AutoReservation.Common.DataTransferObjects;
 using AutoReservation.Dal.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 
-namespace AutoReservation.Dal
-{
-    public class AutoReservationContext
-        : DbContext
-    {
+namespace AutoReservation.Dal {
+    public class AutoReservationContext : DbContext {
 
-        public DbSet<Auto> Autos { get; set; }
-        public DbSet<Kunde> Kunden { get; set; }
-        public DbSet<Reservation> Reservationen { get; set; }
-
-
+        public virtual DbSet<Auto> Autos { get; set; }
+        public virtual DbSet<Kunde> Kunden { get; set; }
+        public virtual DbSet<Reservation> Reservationen { get; set; }
+        
         public static readonly LoggerFactory LoggerFactory = new LoggerFactory(
             new[] { new ConsoleLoggerProvider((_, logLevel) => logLevel >= LogLevel.Information, true) }
         );
 
+        public AutoReservationContext() {
+            if (this.Database.EnsureCreated()) {
+                List<Auto> autos = new List<Auto>() {
+                    new Auto() {
+                        Marke = "Fiat Punto", AutoKlasse = AutoKlasse.Standard, Tagestarif = 50, Basistarif = 0
+                    },
+                    new Auto() {
+                        Marke = "VW Golf", AutoKlasse = AutoKlasse.Mittelklasse, Tagestarif = 120, Basistarif = 0
+                    },
+                    new Auto() {
+                        Marke = "Audi S6", AutoKlasse = AutoKlasse.Luxusklasse, Tagestarif = 180, Basistarif = 50
+                    }
+                };
+
+
+                List<Kunde> kunden = new List<Kunde>() {
+                    new Kunde() { Nachname = "Nass", Vorname = "Anna", Geburtsdatum = Convert.ToDateTime("1961-05-05 00:00:00")},
+                    new Kunde() { Nachname = "Beil", Vorname = "Timo", Geburtsdatum = Convert.ToDateTime("1980-09-09 00:00:00")},
+                    new Kunde() { Nachname = "Pfahl", Vorname = "Marta", Geburtsdatum = Convert.ToDateTime("1950-07-03 00:00:00")},
+                    new Kunde() { Nachname = "Zufall", Vorname = "Rainer", Geburtsdatum = Convert.ToDateTime("1944-11-11 00:00:00")}
+                };
+
+                this.Autos.AddRange(autos);
+                this.Kunden.AddRange(kunden);
+
+                this.Reservationen.AddRange(new List<Reservation>() {
+                    new Reservation() {
+                        Auto = autos[0],
+                        Kunde = kunden[0],
+                        Von = Convert.ToDateTime("2018-05-05 00:00:00"),
+                        Bis = Convert.ToDateTime("2018-10-15 00:00:00")
+                    }
+                });
+
+                this.SaveChanges();
+            }
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured) {
                 optionsBuilder
                     .EnableSensitiveDataLogging()
                     .UseLoggerFactory(LoggerFactory) // Warning: Do not create a new ILoggerFactory instance each time
@@ -32,6 +71,7 @@ namespace AutoReservation.Dal
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
             modelBuilder.Entity<Reservation>()
                 .HasOne<Auto>(reservation => reservation.Auto as Auto)
                 .WithMany(auto => auto.Reservationen)
@@ -45,6 +85,8 @@ namespace AutoReservation.Dal
                 .HasForeignKey(reservation => reservation.KundeId)
                 .HasPrincipalKey(kunde => kunde.Id)
                 .IsRequired();
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
